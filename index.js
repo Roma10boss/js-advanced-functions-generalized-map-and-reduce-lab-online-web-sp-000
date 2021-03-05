4d6b04fac43350076429cef418fc242ec9fef4ce
Romario Gustave <romagustave10@gmail.com>
2021-03-05 06:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 // Add your functions here
-mapToNegativize[]]{
-  return sourceArray.map(x => x * -1);
-}
-
-function mapToNoChange(sourceArray){
-  return sourceArray.map(x => x);
-}
-
-function mapToDouble(sourceArray){
-  return sourceArray.map(x => x * 2);
-}
-
-function mapToSquare(sourceArray){
-return sourceArray.map(x => x * x);
-}
+map([1, 2, 3, -9], function(a){ return a * a }) 
