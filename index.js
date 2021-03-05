3268827f1768dafeda9177e763dac26fb15e282d
Romario Gustave <romagustave10@gmail.com>
2021-03-05 05:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function mapToNegativize(sourceArray){
+  return sourceArray.map(x => x * -1);
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x => x);
+}
+
+function mapToDouble(sourceArray){
+  return sourceArray.map(x => x * 2);
+}
+
+function mapToSquare(sourceArray){
+return sourceArray.map(x => x * x);
+}
